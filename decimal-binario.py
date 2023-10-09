e19000e81c7a84a71abfa6ce856a230e60e7a71d
KOKO <126774488+KOKOSG7@users.noreply.github.com>
2023-10-09 10:24:56 +0000

    first commit

---
+++ decimal-binario.py
@@ -0,0 +1,22 @@
+fin = 0
+while fin == 0:
+    decimal = int(input("Numero: "))
+    if decimal <= 0:
+        decimal = "0"
+    # Aquí almacenamos el resultado
+    binario = ""
+    # Mientras se pueda dividir...
+    while decimal > 0:
+        # Saber si es 1 o 0
+        resto = int(decimal % 2)
+        # E ir dividiendo el decimal
+        decimal = int(decimal / 2)
+        # Ir agregando el número (1 o 0) a la izquierda del resultado
+        binario = str(resto) + binario
+    resultado = binario
+    print(resultado)
+    cont = input("Desea hacer otro calculo? Y/N: ")
+    if cont == "Y":
+        print("------------------------------")
+    elif cont == "N":
+        fin = 1
